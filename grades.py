ba2c7494276e1a60b8dc289bce5ea753cb70f327
Sergei Chestakov <sergei332@gmail.com>
2017-12-28 15:36:47 -0800

    modified function to return the classes a student has taken

--- grades.py
+++ grades.py
@@ -1,6 +1,6 @@
 import psycopg2
-import psycopg2.extras
 
+#Queries the database for the id and major of each student
 def getStudents(cur):
     sql = "SELECT pidm,admit_major_desc FROM students WHERE admit_major_desc IS NOT NULL LIMIT 100"
 
@@ -11,18 +11,24 @@ def getStudents(cur):
         pidm = row[0]
         major = row[1]
         print("pidm: ", row[0], "major: ", row[1])
-        getClasses(pidm)
+        classes = getClasses(pidm)
+        print('Classes: ', classes)
 
+#Queries the database for every class that a student with a certain Id has taken and returns each of them in an array
 def getClasses(pidm):
-    sql = "SELECT * FROM courses WHERE pidm=" + str(pidm)
+    sql = "SELECT subj,crse FROM courses WHERE reg_status_desc='Registered' AND pidm=" + str(pidm)
     try:
         cur.execute(sql)
     except:
         print("INVALID QUERY")
-    classes = cur.fetchall()
-    print("Student's classes: ", classes)
+    courses = cur.fetchall()
+    classes = []
+    for course in courses:
+        name = course[0] + " " + course[1]
+        classes.append(name)
+    return classes
 
-
+#Connect to the database and execute queries
 if __name__ == "__main__":
     conn = psycopg2.connect("dbname='kys_development' user='Sergei' host='localhost' password=''")
     cur = conn.cursor()
