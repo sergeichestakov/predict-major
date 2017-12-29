67712329e4887b7f21503c6fa2f376a8fe1c5a08
Sergei Chestakov <sergei332@gmail.com>
2017-12-28 16:46:25 -0800

    created a class for students and returned it within the function

--- grades.py
+++ grades.py
@@ -1,20 +1,31 @@
 import psycopg2
 
-#Queries the database for the id and major of each student
+class Student:
+    def __init__(self, major, classes):
+        self.major = major
+        self.classes = classes
+
+    def __str__(self):
+        return self.major + " Major. Classes Taken = " + ', '.join(self.classes)
+
+
+#Queries the database for the ID and major of each student
 def getStudents(cur):
-    sql = "SELECT pidm,admit_major_desc FROM students WHERE admit_major_desc IS NOT NULL LIMIT 100"
+    sql = "SELECT pidm,admit_major_desc FROM students WHERE admit_major_desc IS NOT NULL LIMIT 1000"
 
     cur.execute(sql)
     rows = cur.fetchall()
 
+    students = {}
     for row in rows:
         pidm = row[0]
         major = row[1]
-        print("pidm: ", row[0], "major: ", row[1])
-        classes = getClasses(pidm)
-        print('Classes: ', classes)
+        student = Student(major, getClasses(pidm))
+        students[pidm] = student
 
-#Queries the database for every class that a student with a certain Id has taken and returns each of them in an array
+    return students
+
+#Queries the database for every class that a student with a certain ID has taken and returns each of them in an array
 def getClasses(pidm):
     sql = "SELECT subj,crse FROM courses WHERE reg_status_desc='Registered' AND pidm=" + str(pidm)
     try:
@@ -24,7 +35,7 @@ def getClasses(pidm):
     courses = cur.fetchall()
     classes = []
     for course in courses:
-        name = course[0] + " " + course[1]
+        name = " ".join(course)
         classes.append(name)
     return classes
 
@@ -32,4 +43,6 @@ def getClasses(pidm):
 if __name__ == "__main__":
     conn = psycopg2.connect("dbname='kys_development' user='Sergei' host='localhost' password=''")
     cur = conn.cursor()
-    getStudents(cur)
+    students = getStudents(cur)
+    for i in students:
+        print(students[i])
